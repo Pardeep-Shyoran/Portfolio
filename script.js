94856b50b3ea7d03d006b8a2b1654bf61ad6576a
Pardeep Shyoran <143327162+Pardeep-Shyoran@users.noreply.github.com>
2024-12-25 09:12:46 +0530

    #25 Dec

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed('#element', {
         strings: ['Web-Developer.', ' Graphics Designer.', ' Video Editor.', ' Photographer.'],
         typeSpeed: 100,
@@ -17,19 +17,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     const cardWrapper = document.querySelector('.cards');
-        const cards = document.querySelectorAll('.project-card');
+    const cards = document.querySelectorAll('.project-card');
 
-        cards.forEach(card => {
-            const clone = card.cloneNode(true);
-            cardWrapper.appendChild(clone);
-        });
+    cards.forEach(card => {
+        const clone = card.cloneNode(true);
+        cardWrapper.appendChild(clone);
+    });
 
-        const totalWidth = cards.length * (350 + 60);
+    const totalWidth = cards.length * (350 + 60);
 
-        cardWrapper.style.width = `${totalWidth * 2}px`;
+    cardWrapper.style.width = `${totalWidth * 2}px`;
 
-        const animationDuration = cards.length * 3;
-        cardWrapper.style.animationDuration = `${animationDuration}s`;
+    const animationDuration = cards.length * 3;
+    cardWrapper.style.animationDuration = `${animationDuration}s`;
+
+    
 
 
     // Email button click handling
@@ -43,14 +45,14 @@ document.addEventListener('DOMContentLoaded', function() {
     //     window.location.href = mailtoLink;
     // });
 
-    document.getElementById('email-button').addEventListener('click', function() {
+    document.getElementById('email-button').addEventListener('click', function () {
         var recipient = 'pardeep75889@gmail.com';
         var subject = 'Inquiry from Portfolio Website';
         var body = `Hello Pardeep Shyoran,\n\nI visited your portfolio website and I would like to get in touch with you regarding...`;
-    
+
         var mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-    
+
         window.location.href = mailtoLink;
     });
-    
+
 });
